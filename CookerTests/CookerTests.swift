2fc1eca62b7e28b0cd49062e1d976d75e6660d53
omar.zhou <hl_a_k@163.com>
2018-03-21 22:10:12 +0800

    add bailable

--- CookerTests/CookerTests.swift
+++ CookerTests/CookerTests.swift
@@ -24,6 +24,7 @@ class CookerTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        assert(false)
     }
     
     func testPerformanceExample() {
