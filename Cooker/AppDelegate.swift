b360afada6174a51f5ea1624589723d6be0569b7
omar.zhou <hl_a_k@163.com>
2018-03-21 16:36:59 +0800

    add a fake method

--- Cooker/AppDelegate.swift
+++ Cooker/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        returnSomeInteger()
+        
         return true
     }
 
@@ -89,5 +92,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+}
+
+func returnSomeInteger() -> Int {
+    return 10
 }
 
